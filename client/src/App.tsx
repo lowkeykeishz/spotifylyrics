9c35e0543c539b5f36b84695db70e2784f2ef51b
Travis. <youngtravislicious@gmail.com>
2025-05-14 17:50:18 +0000

    added main page and fixed cors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ky from "ky";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
+  const handleLogin = async () => {
+    const response = await ky.get("https://spotifylyrics-six.vercel.app/auth/login");
+    console.log(response);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="bg-base-100 h-screen w-screen flex flex-col items-center justify-center gap-4 md:gap-6">
+      <h1 className="font-semibold text-6xl mb-1 md:text-7xl">Lyricsman.</h1>
+      <p className="w-[28rem] text-center text-base-content/70 max-sm:w-[23rem] md:text-xl md:w-[30rem]">Get the lyrics of Your favorite songs from spotify, in real-time. even the unavailable ones.</p>
+      <button className="btn btn-neutral text-lg md:text-xl md:py-6" onClick={handleLogin}>Connect Your Spotify</button>
+      <footer>
+        <p className="text-sm text-center md:text-lg">Made with ❤️ by <a href="https://www.google.com/" target="_blank">keishz.</a></p>
+      </footer>
+    </main>
+  );
 }
-
-export default App
--- server/main.py
+++ server/main.py
@@ -16,7 +16,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
